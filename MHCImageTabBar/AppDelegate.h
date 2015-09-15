648d4e7157fcb380e27b54238638d6253bc53a81
Mohamed Mohamed <abuobaida@alwans.com>
2015-09-15 07:28:07 +0300

    Initial Commit

---
+++ MHCImageTabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MHCImageTabBar
+//
+//  Created by Mohamed Mohamed on 15/9/15.
+//  Copyright © 2015 MHO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MHCImageTabBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MHCImageTabBar
+//
+//  Created by Mohamed Mohamed on 15/9/15.
+//  Copyright © 2015 MHO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
